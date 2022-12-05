11c2c8ca0bb3134e60ed11148b2c9b7fa6ec5a44
Valerij Ivashchenko <likemusicdev@gmail.com>
2022-12-06 01:28:02 +0300

    Move User-related to "Domain/User" directory.

--- app/Domain/User/UserFactory.php
+++ app/Domain/User/UserFactory.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Database\Factories;
+namespace App\Domain\User;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Domain\User\User>
  */
 class UserFactory extends Factory
 {
