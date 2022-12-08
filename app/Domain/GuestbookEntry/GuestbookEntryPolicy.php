1fc4f79dc548366766ff5a7c71346a09df8d29da
Valerij Ivashchenko <likemusicdev@gmail.com>
2022-12-08 16:14:20 +0300

    Fix GuestbookEntryPolicy.

--- app/Domain/GuestbookEntry/GuestbookEntryPolicy.php
+++ app/Domain/GuestbookEntry/GuestbookEntryPolicy.php
@@ -3,11 +3,23 @@
 namespace App\Domain\GuestbookEntry;
 
 use App\Domain\Base\BasePermissionBasedPolicy;
+use App\Domain\User\User;
+use Illuminate\Database\Eloquent\Model;
 
 class GuestbookEntryPolicy extends BasePermissionBasedPolicy
 {
     protected function getPermissionEntityName(): string
     {
         return GuestbookEntry::PERMISSION_NAME;
+    }
+
+    public function viewAny(?User $user)
+    {
+        return true;
+    }
+
+    public function view(?User $user, Model $model)
+    {
+        return true;
     }
 }
